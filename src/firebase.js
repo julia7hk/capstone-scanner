1394a3a74afb7f4f6bcc35d89a8a0639601caf97
Julia Kang <julia7hk@gmail.com>
2022-11-10 15:21:55 -0800

    connected firebase project

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7fn9eU08LMWr8TyCgAntF5LTZ5ZdP--8",
+  authDomain: "gunnscanner-f243a.firebaseapp.com",
+  projectId: "gunnscanner-f243a",
+  storageBucket: "gunnscanner-f243a.appspot.com",
+  messagingSenderId: "1048232122866",
+  appId: "1:1048232122866:web:5f315349ecd91ed4df336b",
+  measurementId: "G-YKRS0ZD4YB",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
