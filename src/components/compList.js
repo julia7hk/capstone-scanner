834b74a1d22955c5e4e12767672e88d6d121edf2
Julia Kang <julia7hk@gmail.com>
2022-11-14 12:33:38 -0800

    worked on compList and scanner components

--- src/components/compList.js
+++ src/components/compList.js
@@ -1,5 +1,13 @@
 function CompList() {
-  return <h1>CompList</h1>;
+  const students = ["julia", "sara", "sammy"];
+  const listStudents = students.map((student) => <li>{student}</li>);
+
+  return (
+    <div className="border border-red-900 rounded-lg">
+      <h1>CompList</h1>
+      <ul>{listStudents}</ul>
+    </div>
+  );
 }
 
 export default CompList;
--- src/components/scanner.js
+++ src/components/scanner.js
@@ -1,5 +1,10 @@
 function Scanner() {
-  return <h1>Scanner</h1>;
+  return (
+    <div className="border border-red-900 rounded-lg">
+      <h1>Scanner</h1>
+      <input type="text" value="hello" />
+    </div>
+  );
 }
 
 export default Scanner;
