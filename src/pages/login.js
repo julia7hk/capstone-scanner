6e53582785853c3621fdbba7e77d2f77732b2baf
3047322836 <3047322836@qq.com>
2022-11-15 16:11:13 -0800

    deleted space and edit UI

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,14 @@
 function Login() {
   return (
-    <div className="border border-red-900 rounded-lg">
-      <h1>login</h1>
-      <input type="text" value="username" />
-      <input type="password" value="password" />
+    <div className="my-60 mx-40 border border-red-900 rounded-lg">
+      <h1 className="text-3xl text-center">login</h1>
+      <label for = "uname">User Name: </label>
+      <input type="text" id="uname" name="uname" value="username" /><br></br>
+      <label for = "pword">Password: </label>
+      <input type="password" id="pword" name="pword" value="password" /><br></br>
       <button>submit</button>
     </div>
+
   );
 }
 
